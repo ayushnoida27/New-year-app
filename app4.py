d5c869481ba2549ea27cd196dd0d7b19d5970e99
ayushnoida27 <79960180+ayushnoida27@users.noreply.github.com>
2021-03-03 11:18:32 +0530

    Update app4.py
    Changed functn name

--- app4.py
+++ app4.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def function():
     now = datetime.datetime.now()
     new_year = now.month == 1 and now.day == 1
     return render_template("index.html", new_year=new_year)
